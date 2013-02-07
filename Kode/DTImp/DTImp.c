12fdeacddc0c0776345bfcb434fccd0437cf6158
oystesm <oystesm@c6c934be-32c9-4915-a4c0-057c9fb2909a>
2013-02-07 16:32:29 +0000

    Oprettet atmel studio prosjekt
    git-svn-id: svn+ssh://login.stud.ntnu.no/home/shomec/o/oystesm/svn/DTImp@7 c6c934be-32c9-4915-a4c0-057c9fb2909a

---
+++ Kode/DTImp/DTImp.c
@@ -0,0 +1,23 @@
+/*
+ * DTImp.c
+ *
+ * Created: 07.02.2013 17:26:09
+ *  Author: medlem
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
+
+void init(){
+	
+	
+	
+}
